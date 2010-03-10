a5214a65cd959fc9cc48e818bfffd8fd3c3e8dbf
chris <chris@ac150577-6901-3246-a117-cd8547e6756f>
2010-03-10 19:43:21 +0000

    Files() manuel implemented! yay!

--- testfixtures/tests/test_manuel.py
+++ testfixtures/tests/test_manuel.py
@@ -4,12 +4,20 @@
 
 import re
 
-from manuel import Document, Manuel
+from manuel import Document, Region, RegionContainer, Manuel
 from manuel import capture,codeblock
 from mock import Mock
-from testfixtures import compare, Comparison as C
-from testfixtures.manuel import Files,FileBlock,evaluate_file_block,format_code_block
+from testfixtures import compare, Comparison as C, TempDirectory
+from testfixtures.manuel import Files,FileBlock,FileResult
 from unittest import TestCase,makeSuite,TestSuite
+
+class TestContainer(RegionContainer):
+    def __init__(self,attr,*blocks):
+        self.regions = []
+        for block in blocks:
+            region = Region(0,' ')
+            setattr(region,attr,block)
+            self.regions.append(region)
 
 class TestManuel(TestCase):
 
@@ -34,7 +42,7 @@ class TestManuel(TestCase):
   line 6
 
 """)
-        d.parse_with(Files())
+        d.parse_with(Files('td'))
         compare([
                 None,
                 C(FileBlock,
@@ -63,7 +71,7 @@ class TestManuel(TestCase):
 
 This is just some normal text!
 """)
-        d.parse_with(Files())
+        d.parse_with(Files('td'))
         compare([
                 None,
                 C(FileBlock,
@@ -83,7 +91,7 @@ This is just some normal text!
   out = 'there'
 
 """)
-        d.parse_with(Files())
+        d.parse_with(Files('td'))
         compare([r.parsed for r in d],[None])
 
     def test_no_class(self):
@@ -94,7 +102,7 @@ This is just some normal text!
   out = 'there'
 
 """)
-        d.parse_with(Files())
+        d.parse_with(Files('td'))
         compare([r.parsed for r in d],[None])
     
     def test_unclaimed_works(self):
@@ -119,7 +127,7 @@ This is just some normal text!
   line 3
 
 """)
-        d.parse_with(Files()+Test())
+        d.parse_with(Files('td')+Test())
         # now check FileBlock didn't mask class block
         compare([
                 None,
@@ -128,18 +136,74 @@ This is just some normal text!
                 None,
                 ],[r.parsed for r in d])
 
-    def test_evaluate_read(self):
-        pass
+    def test_evaluate_non_fileblock(self):
+        m = Mock()
+        d = TestContainer('parsed',m)
+        d.evaluate_with(Files('td'),globs={})
+        compare([None],[r.evaluated for r in d])
+        compare(m.call_args_list,[])
+        compare(m.method_calls,[])
+
+    def test_evaluate_read_same(self):
+        dir = TempDirectory()
+        dir.write('foo','content')
+        d = TestContainer('parsed',FileBlock('foo','content','read'))
+        d.evaluate_with(Files('td'),globs={'td':dir})
+        compare([C(FileResult,
+                   passed=True,
+                   expected=None,
+                   actual=None)],
+                [r.evaluated for r in d])
+
+    def test_evaluate_read_difference(self):
+        dir = TempDirectory()
+        dir.write('foo','actual')
+        d = TestContainer('parsed',FileBlock('foo','expected','read'))
+        d.evaluate_with(Files('td'),globs={'td':dir})
+        compare([C(FileResult,
+                   passed=False,
+                   path='foo',
+                   expected='expected',
+                   actual='actual')],
+                [r.evaluated for r in d])
 
     def test_evaulate_write(self):
-        pass
+        dir = TempDirectory()
+        d = TestContainer('parsed',FileBlock('foo','content','write'))
+        d.evaluate_with(Files('td'),globs={'td':dir})
+        compare([C(FileResult,
+                   passed=True,
+                   expected=None,
+                   actual=None)],
+                [r.evaluated for r in d])
+        dir.check('foo')
+        compare(dir.read('foo'),'content')
 
-    def test_formatter_write(self):
-        pass
+    def test_formatter_non_fileblock(self):
+        d = TestContainer('evaluated',object)
+        d.format_with(Files('td'))
+        compare(d.formatted(),'')
 
-    def test_formatter_read_ok(self):
-        pass
+    def test_formatter_passed(self):
+        d = TestContainer('evaluated',FileResult())
+        d.format_with(Files('td'))
+        compare(d.formatted(),'')
 
-    def test_formatter_read_fail(self):
-        pass
+    def test_formatter_failed(self):
+        r = FileResult()
+        r.passed = False
+        r.path = '/foo/bar'
+        r.expected = 'same\nexpected\n'
+        r.actual = 'same\nactual\n'
+        d = TestContainer('evaluated',r)
+        d.format_with(Files('td'))
+        compare('File "<memory>", line 0:\n'
+                'Reading from "/foo/bar":\n'
+                '@@ -1,3 +1,3 @@\n'
+                ' same\n'
+                '-expected\n'
+                '+actual\n ',
+                d.formatted()
+                )
+
     
