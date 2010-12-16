7820dcb209dea92ec0dd2e8c7faa979c1ab5ba5d
chris <chris@ac150577-6901-3246-a117-cd8547e6756f>
2010-12-16 14:42:54 +0000

    Manuel File plugin documented - yay\!

---
+++ testfixtures/tests/test_manuel_examples.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2010 Simplistix Ltd
+#
+# See license.txt for more details.
+from os.path import dirname
+path_to_your_docs = dirname(__file__)
+
+from glob import glob
+from manuel import doctest,codeblock,capture
+from manuel.testing import TestSuite
+from os.path import join
+from testfixtures import TempDirectory
+from testfixtures.manuel import Files
+
+def setUp(test):
+    test.globs['tempdir']=TempDirectory()
+
+def tearDown(test): 
+    test.globs['tempdir'].cleanup()
+
+def test_suite():
+    m = doctest.Manuel()
+    m += codeblock.Manuel()
+    m += capture.Manuel()
+    m += Files('tempdir')
+    return TestSuite(
+        m,
+        setUp=setUp,
+        tearDown=tearDown,
+        *glob(join(path_to_your_docs,'*.txt'))
+        )
