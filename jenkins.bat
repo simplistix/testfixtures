beaa0a2fe91caaafce7aa3089f611747256b508c
Chris Withers <chris@simplistix.co.uk>
2012-01-26 16:11:23 +0000

    Turn on branch coverage reporting and the required html reports.

--- jenkins.bat
+++ jenkins.bat
@@ -1,5 +1,5 @@
 %PYTHON_EXE% bootstrap.py
 CHOICE /T 20 /C w /D w
 bin\buildout
-bin\nosetests --with-xunit --with-cov --cov=testfixtures --cov-report=xml
+bin\nosetests --with-xunit --with-cov --cov=testfixtures --cov-report=xml --cov-report=html
 bin\docpy setup.py sdist
