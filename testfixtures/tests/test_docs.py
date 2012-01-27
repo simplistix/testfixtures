0230c94110e99f31aea413230a908bae8cce467d
Chris Withers <chris@simplistix.co.uk>
2012-01-27 09:22:25 +0000

    Allow docs to test to be found elsewhere. (they're not unpacked by installing the sdist)

--- testfixtures/tests/test_docs.py
+++ testfixtures/tests/test_docs.py
@@ -6,15 +6,21 @@ from doctest import REPORT_NDIFF,ELLIPSIS
 from glob import glob
 from manuel import doctest, capture
 from manuel.testing import TestSuite
-from os.path import dirname,join,pardir
+from nose.plugins.skip import SkipTest
+from os.path import dirname, join, pardir
+
+import os
 
 from . import compat
+
+path = os.environ.get('DOCPATH', join(dirname(__file__),pardir,pardir,'docs'))
+tests = glob(join(path,'*.txt'))
+
+if not tests:
+    raise SkipTest('No docs found to test') # pragma: no cover
 
 def test_suite():
     m =  doctest.Manuel(optionflags=REPORT_NDIFF|ELLIPSIS)
     m += compat.Manuel()
     m += capture.Manuel()
-    return TestSuite(
-        m,
-        *glob(join(dirname(__file__),pardir,pardir,'docs','*.txt'))
-        )
+    return TestSuite(m, *tests)
