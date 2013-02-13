22aae8f25da1e00c61b3c7765ef2aa29ab364605
Chris Withers <chris@simplistix.co.uk>
2013-02-13 18:51:23 +0000

    Do coverage in a downstream job.

--- jenkins.bat
+++ jenkins.bat
@@ -1,5 +1,5 @@
 %PYTHON_EXE% bootstrap.py
 CHOICE /T 20 /C w /D w
 bin\buildout
-bin\nosetests --with-xunit --with-cov --cov=testfixtures --cov-report=xml --cov-report=html
+bin\nosetests --with-xunit --with-cov --cov=testfixtures
 bin\docpy setup.py sdist
