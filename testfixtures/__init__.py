55f7454c0a631cb04cb51e5fc75cf545abcb715d
Chris Withers <chris@simplistix.co.uk>
2013-02-20 13:26:05 +0000

    remove commented chunk.

--- testfixtures/__init__.py
+++ testfixtures/__init__.py
@@ -1,18 +1,6 @@
 # Copyright (c) 2008-2011 Simplistix Ltd
 # See license.txt for license details.
 
-## import sys
-
-## from calendar import timegm
-## from cStringIO import StringIO
-## from datetime import datetime,timedelta,date
-## from difflib import unified_diff
-## from functools import partial,wraps
-## from new import classobj
-## from pprint import pformat
-## 
-## from types import ClassType,GeneratorType,MethodType
-
 class singleton(object):
     def __init__(self, name):
         self.name = name
