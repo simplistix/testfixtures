1fbb555185ce17d77c82c5c7f4d25692d0b4fd9d
Chris Withers <chris@simplistix.co.uk>
2013-02-21 08:08:25 +0000

    Stable import for byte-type and unicode-type.

--- testfixtures/compat.py
+++ testfixtures/compat.py
@@ -5,29 +5,34 @@ if sys.version_info[:2] > (3, 0):
 
     PY2 = False
     PY3 = True
-    
+
+    Bytes = bytes
+    Unicode = str
     basestring = str
+
     class_type_name = 'class'
     ClassType = type
     exception_module = 'builtins'
     new_class = type
     self_name = '__self__'
     from io import StringIO
-    Unicode = str
     xrange = range
     
 else:
     
-    PY3 = False
     PY2 = True
+    PY3 = False
     
+    Bytes = str
+    Unicode = unicode
     basestring = basestring
+    
     class_type_name = 'type'
     from types import ClassType
     exception_module = 'exceptions'
     from new import classobj as new_class
     self_name = 'im_self'
     from cStringIO import StringIO
-    Unicode = unicode
     xrange = xrange
+
     
