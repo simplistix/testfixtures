2502a979cf797d176b5949af3a5404ec03a4b80a
Chris Withers <chris@simplistix.co.uk>
2014-03-31 23:26:16 +0100

    mark up coverage paths that we hope will never execute

--- testfixtures/rmtree.py
+++ testfixtures/rmtree.py
@@ -19,7 +19,7 @@ if sys.platform.startswith("win"):
         # Testing on a i7@4.3GHz shows that usually only 1 iteration is
         # required when contention occurs.
         timeout = 0.001
-        while timeout < 1.0:
+        while timeout < 1.0: # pragma: no branch
             # Note we are only testing for the existence of the file(s) in
             # the contents of the directory regardless of any security or
             # access rights.  If we have made it this far, we have sufficient
@@ -28,12 +28,12 @@ if sys.platform.startswith("win"):
             # Other Windows APIs can fail or give incorrect results when
             # dealing with files that are pending deletion.
             L = os.listdir(dirname)
-            if not (L if waitall else name in L):
+            if not (L if waitall else name in L): # pragma: no branch
                 return
             # Increase the timeout and try again
-            time.sleep(timeout)
-            timeout *= 2
-        warnings.warn('tests may fail, delete still pending for ' + pathname,
+            time.sleep(timeout) # pragma: no cover
+            timeout *= 2 # pragma: no cover
+        warnings.warn('tests may fail, delete still pending for ' + pathname, # pragma: no cover
                       RuntimeWarning, stacklevel=4)
 
     def _rmtree(path):
@@ -53,7 +53,7 @@ else:
 def rmtree(path):
     try:
         _rmtree(path)
-    except OSError as e:
+    except OSError as e: # pragma: no cover
         # Unix returns ENOENT, Windows returns ESRCH.
-        if e.errno not in (errno.ENOENT, errno.ESRCH):
+        if e.errno not in (errno.ENOENT, errno.ESRCH): # pragma: no branch
             raise
