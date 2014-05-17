e6544379fd6b126d02abe6d7dbe52434a2783069
Chris Withers <chris@simplistix.co.uk>
2014-05-17 20:31:38 +0100

    Factor out duplicate parameter documentation.

--- testfixtures/shouldraise.py
+++ testfixtures/shouldraise.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2008-2013 Simplistix Ltd
+# Copyright (c) 2008-2014 Simplistix Ltd
 # See license.txt for license details.
 
 from functools import partial, wraps
@@ -39,10 +39,7 @@ class _should_raise:
     def __call__(self,*args,**kw):
         return ShouldRaiseWrapper(self, partial(self.callable))(*args, **kw)
 
-class ShouldRaise:
-    """
-    This context manager is used to assert that an exception is raised
-    within the context it is managing.
+param_docs = """
 
     :param exception: This can be one of the following:
     
@@ -56,7 +53,13 @@ class ShouldRaise:
                       * An exception instance, indicating that an
                         exception exactly matching the one supplied
                         should be raised.  
+"""
+
+class ShouldRaise:
     """
+    This context manager is used to assert that an exception is raised
+    within the context it is managing.
+    """ + param_docs
 
     #: The exception captured by the context manager.
     #: Can be used to inspect specific attributes of the exception.
@@ -83,22 +86,8 @@ class should_raise(object):
     an exception. An exception class or exception instance may be
     passed to check more specifically exactly what exception will be
     raised.
-
-    :param exception: This can be one of the following:
-    
-                      * `None`, indicating that an exception must be
-                        raised, but the type is unimportant.
-                        
-                      * An exception class, indicating that the type
-                        of the exception is important but not the
-                        parameters it is created with.
-                        
-                      * An exception instance, indicating that an
-                        exception exactly matching the one supplied
-                        should be raised.  
-    
-    """
-
+    """ + param_docs
+        
     # backwards compatibility for the old should_raise
     # wrapper stuff
     def __new__(cls,target_or_exception, exception=None):
