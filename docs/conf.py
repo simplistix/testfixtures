6c95737e74f087247c91939e2ce12ebaac237e02
Chris Withers <chris@simplistix.co.uk>
2016-02-18 22:22:54 +0000

    silence warning.

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = pkg_info.name
 copyright = '2008-%s Simplistix Ltd' % datetime.datetime.now().year
 version = release = pkg_info.version
 exclude_trees = ['_build']
-unused_docs = ['description']
+unused_docs = ['description.txt']
 pygments_style = 'sphinx'
 
 # Options for HTML output
