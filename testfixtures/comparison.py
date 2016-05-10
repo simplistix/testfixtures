89416427241043331beac31f0d9cf079ccc8ad6c
Asaf Peleg <asaf@locusenergy.com>
2016-05-10 15:42:35 -0400

    Adding in documentation for Range Comparison since all functionality has been reviewed.

--- testfixtures/comparison.py
+++ testfixtures/comparison.py
@@ -691,6 +691,12 @@ class RangeComparison:
     """
     An object that can be used in comparisons of numerics to validate
     actual falls within a certain range
+
+    :param lower_bound: the numeric that the actual has to be equal
+                        to or greater than
+
+    :param upper_bound: the numeric that the actual has to be equal
+                        to or less than
     """
     def __init__(self, lower_bound, upper_bound):
         self.lower_bound = lower_bound
