74b989ba616d7c0346b346848fac0a5a1ff9e53d
Chris Withers <chris@simplistix.co.uk>
2017-04-13 08:29:22 +0100

    finally retire ancient doctests and re-write them as shiny new pytest-ish tests.

--- testfixtures/tests/test_replacer.py
+++ testfixtures/tests/test_replacer.py
@@ -1,203 +1,155 @@
 # Copyright (c) 2008-2013 Simplistix Ltd
 # See license.txt for license details.
 
-from doctest import DocTestSuite, REPORT_NDIFF, ELLIPSIS
-from testfixtures import Replacer
-from unittest import TestSuite
+from testfixtures import Replacer, ShouldRaise
 
 
 class TestReplacer:
 
-    def test_function(self):  # pragma: no branch
-        """
-        >>> from testfixtures.tests import sample1
-        >>> sample1.z()
-        'original z'
+    def test_function(self):
+        from testfixtures.tests import sample1
+        assert sample1.z() == 'original z'
 
-        >>> def test_z():
-        ...   return 'replacement z'
+        def test_z():
+          return 'replacement z'
 
-        >>> r = Replacer()
-        >>> r.replace('testfixtures.tests.sample1.z',test_z)
+        r = Replacer()
+        r.replace('testfixtures.tests.sample1.z',test_z)
 
-        >>> sample1.z()
-        'replacement z'
+        assert sample1.z() == 'replacement z'
 
-        >>> r.restore()
+        r.restore()
 
-        >>> sample1.z()
-        'original z'
-        """
+        assert sample1.z() == 'original z'
 
-    def test_class(self):  # pragma: no branch
-        """
-        >>> from testfixtures.tests import sample1
-        >>> sample1.X()
-        <testfixtures.tests.sample1.X ...>
+    def test_class(self):
+        from testfixtures.tests import sample1
+        x = sample1.X()
+        assert x.__class__.__name__ == 'X'
 
-        >>> class XReplacement(sample1.X): pass
+        class XReplacement(sample1.X): pass
 
-        >>> r = Replacer()
-        >>> r.replace('testfixtures.tests.sample1.X', XReplacement)
+        r = Replacer()
+        r.replace('testfixtures.tests.sample1.X', XReplacement)
 
-        >>> sample1.X()
-        <testfixtures.tests.test_replacer.XReplacement ...>
-        >>> sample1.X().y()
-        'original y'
+        x = sample1.X()
+        assert x.__class__.__name__ == 'XReplacement'
+        assert sample1.X().y() == 'original y'
 
-        >>> r.restore()
+        r.restore()
 
-        >>> sample1.X()
-        <testfixtures.tests.sample1.X ...>
-        """
+        x = sample1.X()
+        assert x.__class__.__name__ == 'X'
 
-    def test_method(self):  # pragma: no branch
-        """
-        >>> from testfixtures.tests import sample1
-        >>> sample1.X().y()
-        'original y'
+    def test_method(self):
+        from testfixtures.tests import sample1
+        assert sample1.X().y() == 'original y'
 
-        >>> def test_y(self):
-        ...   return 'replacement y'
+        def test_y(self):
+          return 'replacement y'
 
-        >>> r = Replacer()
-        >>> r.replace('testfixtures.tests.sample1.X.y',test_y)
+        r = Replacer()
+        r.replace('testfixtures.tests.sample1.X.y',test_y)
 
-        >>> sample1.X().y()[:38]
-        'replacement y'
+        assert sample1.X().y()[:38] == 'replacement y'
 
-        >>> r.restore()
+        r.restore()
 
-        >>> sample1.X().y()
-        'original y'
-        """
+        assert sample1.X().y() == 'original y'
 
-    def test_class_method(self):  # pragma: no branch
-        """
-        >>> from testfixtures.tests import sample1
-        >>> sample1.X.aMethod()
-        <class ...testfixtures.tests.sample1.X...>
+    def test_class_method(self):
+        from testfixtures.tests import sample1
+        c = sample1.X
+        assert sample1.X.aMethod() is c
 
-        >>> def rMethod(cls):
-        ...   return (cls,1)
+        def rMethod(cls):
+          return cls, 1
 
-        >>> r = Replacer()
-        >>> r.replace('testfixtures.tests.sample1.X.aMethod',rMethod)
+        r = Replacer()
+        r.replace('testfixtures.tests.sample1.X.aMethod',rMethod)
 
-        >>> sample1.X.aMethod()
-        (<class ...testfixtures.tests.sample1.X...>, 1)
+        sample1.X.aMethod()
+        assert sample1.X.aMethod() == (c, 1)
 
-        >>> r.restore()
+        r.restore()
 
-        >>> sample1.X.aMethod()
-        <class ...testfixtures.tests.sample1.X...>
-        """
+        sample1.X.aMethod()
+        assert sample1.X.aMethod() is c
 
-    def test_multiple_replace(self):  # pragma: no branch
-        """
-        >>> from testfixtures.tests import sample1
-        >>> sample1.z()
-        'original z'
-        >>> sample1.X().y()
-        'original y'
+    def test_multiple_replace(self):
+        from testfixtures.tests import sample1
+        assert sample1.z() == 'original z'
+        assert sample1.X().y() == 'original y'
 
-        >>> def test_y(self):
-        ...   return repr(self)
-        >>> def test_z():
-        ...   return 'replacement z'
+        def test_y(self):
+          return self.__class__.__name__
+        def test_z():
+          return 'replacement z'
 
-        >>> r = Replacer()
-        >>> r.replace('testfixtures.tests.sample1.z',test_z)
-        >>> r.replace('testfixtures.tests.sample1.X.y',test_y)
+        r = Replacer()
+        r.replace('testfixtures.tests.sample1.z',test_z)
+        r.replace('testfixtures.tests.sample1.X.y',test_y)
 
-        >>> sample1.z()
-        'replacement z'
-        >>> sample1.X().y()
-        '<testfixtures.tests.sample1.X ...>'
+        assert sample1.z() == 'replacement z'
+        assert sample1.X().y() == 'X'
 
-        >>> r.restore()
+        r.restore()
 
-        >>> sample1.z()
-        'original z'
-        >>> sample1.X().y()
-        'original y'
-        """
+        assert sample1.z() == 'original z'
+        assert sample1.X().y() == 'original y'
 
-    def test_gotcha(self):  # pragma: no branch
-        """
-        Just because you replace an object in one context:
+    def test_gotcha(self):
+        # Just because you replace an object in one context:
 
-        >>> from testfixtures.tests import sample1
-        >>> from testfixtures.tests import sample2
-        >>> sample1.z()
-        'original z'
+        from testfixtures.tests import sample1
+        from testfixtures.tests import sample2
+        assert sample1.z() == 'original z'
 
-        >>> def test_z():
-        ...   return 'replacement z'
+        def test_z():
+          return 'replacement z'
 
-        >>> r = Replacer()
-        >>> r.replace('testfixtures.tests.sample1.z',test_z)
+        r = Replacer()
+        r.replace('testfixtures.tests.sample1.z',test_z)
 
-        >>> sample1.z()
-        'replacement z'
+        assert sample1.z() == 'replacement z'
 
-        Doesn't meant that it's replaced in all contexts:
+        # Doesn't meant that it's replaced in all contexts:
 
-        >>> sample2.z()
-        'original z'
+        assert sample2.z() == 'original z'
 
-        >>> r.restore()
-        """
+        r.restore()
 
-    def test_remove_called_twice(self):  # pragma: no branch
-        """
-        >>> from testfixtures.tests import sample1
+    def test_remove_called_twice(self):
+        from testfixtures.tests import sample1
 
-        >>> def test_z():
-        ...   return 'replacement z'
+        def test_z():
+          return 'replacement z'
 
-        >>> r = Replacer()
-        >>> r.replace('testfixtures.tests.sample1.z',test_z)
+        r = Replacer()
+        r.replace('testfixtures.tests.sample1.z',test_z)
 
-        >>> r.restore()
-        >>> sample1.z()
-        'original z'
+        r.restore()
+        assert sample1.z() == 'original z'
 
-        >>> r.restore()
-        >>> sample1.z()
-        'original z'
-        """
+        r.restore()
+        assert sample1.z() == 'original z'
 
-    def test_with_statement(self):  # pragma: no branch
-        """
-        >>> from testfixtures.tests import sample1
-        >>> sample1.z()
-        'original z'
+    def test_with_statement(self):
+        from testfixtures.tests import sample1
+        assert sample1.z() == 'original z'
 
-        >>> def test_z():
-        ...   return 'replacement z'
+        def test_z():
+          return 'replacement z'
 
-        >>> with Replacer() as r:
-        ...   r.replace('testfixtures.tests.sample1.z',test_z)
-        ...   sample1.z()
-        'replacement z'
+        with Replacer() as r:
+            r.replace('testfixtures.tests.sample1.z',test_z)
+            assert sample1.z() == 'replacement z'
 
-        >>> sample1.z()
-        'original z'
-        """
+        assert sample1.z() == 'original z'
 
-    def test_not_there(self):  # pragma: no branch
-        """
-        >>> def test_bad():
-        ...   return 'moo'
+    def test_not_there(self):
+        def test_bad(): pass
 
-        >>> with Replacer() as r:
-        ...   r.replace('testfixtures.tests.sample1.bad',test_bad)
-        Traceback (most recent call last):
-        ...
-        AttributeError: Original 'bad' not found
-        """
-
-
-def test_suite():
-    return DocTestSuite(optionflags=REPORT_NDIFF | ELLIPSIS)
+        with Replacer() as r:
+            with ShouldRaise(AttributeError("Original 'bad' not found")):
+                r.replace('testfixtures.tests.sample1.bad', test_bad)
