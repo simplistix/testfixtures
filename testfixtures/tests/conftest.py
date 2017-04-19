4a2a451325b34d1c7f8b0c80b6b1aa1e8e36d025
Chris Withers <chris@simplistix.co.uk>
2017-04-19 18:20:50 +0100

    Switch to sybil for file reading/writing.
    ...still need to update the docs for this!

---
+++ testfixtures/tests/conftest.py
@@ -0,0 +1,22 @@
+from sybil import Sybil, DocTestParser, CodeBlockParser, parse_captures
+from testfixtures import TempDirectory
+from testfixtures.sybil import FileParser
+
+def sybil_setup(namespace):
+    namespace['tempdir'] = TempDirectory()
+
+
+def sybil_teardown(namespace):
+    namespace['tempdir'].cleanup()
+
+
+pytest_collect_file = Sybil(
+    parsers=[
+        DocTestParser(),
+        CodeBlockParser(),
+        parse_captures,
+        FileParser('tempdir'),
+    ],
+    pattern='*.txt',
+    setup=sybil_setup, teardown=sybil_teardown,
+).pytest()
