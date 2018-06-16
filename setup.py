888e5240b54447b8a37e963a959a0615c8e91a51
Chris Withers <chris@simplistix.co.uk>
2018-06-16 17:22:20 +0100

    fix pytest requirement to work around pip's crappy dependency solver

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     zip_safe=False,
     include_package_data=True,
     extras_require=dict(
-        test=['pytest', 'mock', 'sybil', 'coverage', 'coveralls',
+        test=['pytest>=3.6', 'mock', 'sybil', 'coverage', 'coveralls',
               'zope.component',
               'django<2;python_version<"3"',
               'django;python_version>="3"',
