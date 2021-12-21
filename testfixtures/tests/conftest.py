1ed037629811839761ca00b335661da37ee93963
Ben Greiner <code@bnavigator.de>
2021-12-21 18:46:13 +0100

    Use sybil 3 name

--- testfixtures/tests/conftest.py
+++ testfixtures/tests/conftest.py
@@ -1,6 +1,6 @@
 from sybil import Sybil
 from sybil.parsers.doctest import  DocTestParser
-from sybil.parsers.codeblock import CodeBlockParser
+from sybil.parsers.codeblock import PythonCodeBlockParser
 from sybil.parsers.capture import parse_captures
 
 from testfixtures import TempDirectory
@@ -18,7 +18,7 @@ def sybil_teardown(namespace):
 pytest_collect_file = Sybil(
     parsers=[
         DocTestParser(),
-        CodeBlockParser(),
+        PythonCodeBlockParser(),
         parse_captures,
         FileParser('tempdir'),
     ],
