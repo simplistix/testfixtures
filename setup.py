c2859dbc1f23c63a72567a508a53d7893d5c564b
Chris Withers <chris@withers.org>
2025-07-03 13:27:58 +0100

    Move classifier that build now complains about

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
     packages=find_packages(),
