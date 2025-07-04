c8973451ec7563c153aba745a17655e9a6cfdb9d
Chris Withers <chris@withers.org>
2025-07-04 15:17:54 +0100

    MockedCurrent is an implementation detail
    We don't want this showing up in docs, so mask warning about it being not found

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ nitpick_ignore = [
     ('py:class', 'django.db.models.base.Model'),  # not documented upstream
     ('py:class', 'module'),  # ModuleType not documented.
     ('py:class', 'tempfile.TemporaryFile'),  # not documented as a class so type annotation broken
+    ('py:class', 'testfixtures.datetime.MockedCurrent'),  # internal class that shouldn't be doc'ed
     ('py:class', 'testfixtures.replace.R'),  # type var
     ('py:class', 'testfixtures.utils.T'),  # type var
     ('py:class', 'testfixtures.utils.U'),  # type var
