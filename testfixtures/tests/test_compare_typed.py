a67535f1b99514d9380a1f44db5c760dc0eea3fb
Chris Withers <chris@withers.org>
2025-10-08 08:58:17 +0100

    Implement sequence()

--- testfixtures/tests/test_compare_typed.py
+++ testfixtures/tests/test_compare_typed.py
@@ -1,8 +1,9 @@
 # Tests that ensure compare and Comparison work correctly in strictly type checked environments
 from dataclasses import dataclass
+from typing import Sequence
 
 from testfixtures import compare, Comparison
-from testfixtures.comparison import like
+from testfixtures.comparison import like, sequence
 
 
 @dataclass
@@ -13,6 +14,17 @@ class SampleClass:
 
 class OtherClass:
     pass
+
+
+@dataclass
+class ListCollection:
+    items: list[SampleClass]
+
+
+@dataclass
+class TupleCollection:
+    items: tuple[SampleClass, ...]
+
 
 def test_simple_compare() -> None:
     compare(SampleClass(1, '2'), expected=SampleClass(1, '2'))
@@ -36,3 +48,59 @@ def test_comparison_via_like_in_assert() -> None:
     assert expected == SampleClass(3, '4')
     assert expected != SampleClass
     assert expected != OtherClass()
+
+
+class TestSequence:
+    def test_minimal(self) -> None:
+        actual = ListCollection([SampleClass(1, '2')])
+        compare(actual, expected=ListCollection(sequence()([SampleClass(1, '2')])))
+
+    def test_maximal(self) -> None:
+        actual = ListCollection(
+            [SampleClass(1, 'x'), SampleClass(2, 'x'), SampleClass(3, 'x')]
+        )
+        compare(
+            actual,
+            expected=ListCollection(
+                sequence(partial=True, ordered=False, recursive=False)(
+                    [
+                        SampleClass(3, 'x'),
+                        SampleClass(2, 'x'),
+                    ],
+                )
+            ),
+        )
+
+    def test_minimal_type_override(self) -> None:
+        actual = TupleCollection((SampleClass(1, '2'),))
+        compare(
+            actual,
+            expected=TupleCollection(
+                sequence(returns=tuple[SampleClass, ...])([SampleClass(1, '2')])
+            ),
+        )
+
+    def test_maximal_type_override(self) -> None:
+        actual = TupleCollection(
+            (
+                SampleClass(1, 'x'),
+                SampleClass(2, 'x'),
+                SampleClass(3, 'x'),
+            )
+        )
+        compare(
+            actual,
+            expected=TupleCollection(
+                sequence(
+                    partial=True,
+                    ordered=False,
+                    recursive=False,
+                    returns=tuple[SampleClass, ...],
+                )(
+                    [
+                        SampleClass(3, 'x'),
+                        SampleClass(2, 'x'),
+                    ]
+                )
+            ),
+        )
