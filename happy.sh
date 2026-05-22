fa06debef6d772baf25efdf97680e91b24bb41f8
Chris Withers <chris@withers.org>
2026-05-22 19:34:36 +0100

    Happier coverage checking
    Don't exclude tests, cut down noise from covered!

--- happy.sh
+++ happy.sh
@@ -5,7 +5,7 @@ echo "=== Syncing dependencies ==="
 uv sync --all-extras --all-groups
 
 echo "=== Tests + Coverage ==="
-uv run pytest --cov=testfixtures --cov-report=term-missing
+uv run pytest --cov --cov-report=term-missing:skip-covered
 
 echo "=== Type Checking ==="
 uv run mypy .
